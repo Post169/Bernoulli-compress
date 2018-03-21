60071b281c63d3ffc8b395320ef068dffcceb52c
David N <dcngenmail@gmail.com>
2018-03-20 20:36:54 -0500

    Adding functionality, saving midstep Added more dunder methods Added encode and decode methods In process of making encode work, modified checkDict, which no longer works for __init__; must be fixed decode is not correctly structured, must be rewritten

--- BernASC.py
+++ BernASC.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def BernSeq(length,freq):
     seq = ' '*length
-    print seq
+    #print seq
     for ii in range(length):
         num = np.random.rand(1)
         if num < freq:
@@ -17,9 +17,8 @@ def BernSeq(length,freq):
         else:
             cha = '0'
         seq = seq[:ii] + cha + seq[ii+1:]
-    print seq
+    #print seq
     return seq
-
 
 def dictWords(dataLength):
     from numpy import ceil
@@ -33,20 +32,16 @@ def dictWords(dataLength):
     totalWords = 2**(maxLength+1) - 2
     extraWords = ceil(extraLength/maxLength)
     finalWords = totalWords - extraWords
-#    print "Length of the data is ", dataLength
-#    print "Sum of length of all words is no more than ", sumLengths
-#    print "That is ", extraLength, " bits too long"
-#    print "That is a total of ", totalWords, " words"
-#    print "That's ", extraWords, " more than is needed"
-#    print "We need no more than ", finalWords, " words"
     return finalWords
 
-def checkDict(data,webster,ii):
+def checkDict(data,webster,ii,cut=True):
     "Input dataset building dictionary from, dictionary built so far, current"
     km = 1
     while data[ii:ii+km] in webster:
         km += 1
-        if ii+km > len(data):
+        if km%5 == 0:
+            print km
+        if cut & ii+km > len(data):
             return "", km
     return data[ii:ii+km], km
 
@@ -56,30 +51,6 @@ def binWords(length, howMany):
     howMany = int(howMany)
     for ii in range(howMany):
         yield bin(ii)[2:].zfill(length)
-
-#bitz = BernSeq(20,0.4)
-#bDL = dictWords(len(bitz))
-#wL = np.ceil(np.log2(bDL))
-#webster = {}
-#ii = 0
-#for woid in binWords(wL,bDL):
-#    if ii > len(bitz):
-#        break
-#    nextW,di = checkDict(bitz,webster,ii)
-#    ii += di
-#    webster[nextW] = woid
-
-#while ii < bDL:
-#    print(ii)
-#    nextW,di = checkDict(bitz,webster,ii)
-#    ii += di
-#    webster[nextW] = binWords(wL,bDL)
-    
-#dl75 = dictWords(75)
-#dl76 = dictWords(76)
-#dl77 = dictWords(77)
-#dl78 = dictWords(78)
-#dl79 = dictWords(79)
 
 class LZ78dict(object):
     
@@ -96,5 +67,37 @@ class LZ78dict(object):
             bitno += bitstep
             self.encodeDict[nextW] = ward
         self.decodeDict = {v: k for k, v in self.encodeDict.iteritems()}
-    
-        
+    def __len__(self):
+        return len(self.decodeDict)
+    def __repr__(self):
+        return "Encode dictionary: ",self.encodeDict,"Decode dictionary: ",\
+    self.decodeDict
+    def __str__(self):
+        return repr(self)
+    def encode(self,message):
+        ii = 0
+        kryptos = ""
+        while ii < len(message):
+            ward,di = checkDict(message,self.encodeDict,ii,False)
+            kryptos += ward
+        return kryptos
+    def decode(self,coded):
+        ii = 0
+        original = ""
+        while ii < len(coded):
+            ward,di = checkDict(coded,self.decodeDict,ii,False)
+            original += ward
+        return original
+
+dlen128cww = 0 # put datalength needed to require 128 code words here
+dictlen = 0 # keep track of the dictionary length here
+while dictlen < 128:
+    dlen128cww += 1
+    dictlen = dictWords(dlen128cww)
+print dlen128cww
+sample60 = BernSeq(dlen128cww,.6) # Bernoulli sequence to make a 128 word dict
+print len(sample60)
+webster = LZ78dict(sample60)
+print len(webster)
+coded_sample60 = webster.encode(sample60)
+print coded_sample60
