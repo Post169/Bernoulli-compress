c8e9917d0e7b7ff36fd8d694c8eaae95e0dc8001
David N <dcngenmail@gmail.com>
2018-03-26 12:41:43 -0500

    Refined docstrings

--- bernoulli_lab.py
+++ bernoulli_lab.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 21 14:34:38 2018
-Recreating the functionality of Matlab code developed in MS thesis project
+Recreating the functionality of Matlab code developed in M.S. thesis project
 Takes bitstring, makes dictionary and both encodes and decodes by that
 dictionary; also includes functions to help utilize that, including creating 
 Bernoulli Sequences
@@ -9,15 +9,15 @@ Bernoulli Sequences
 """
 import numpy as np
 
-def bern_seq(length,freq):
-    """Create a Bernoulli sequence - a random bitstring - of the given length 
-    and with the given frequency of 1s"""
-    seq = "".join("1" if np.random.rand(1) < freq else "0" for _ in range(length))
+def bern_seq(length,freq1s):
+    """bern_seq creates Bernoulli sequence (random bitstring) of given length
+    with given freq of 1s"""
+    seq = "".join("1" if np.random.rand(1) < freq1s else "0" for _ in range(length))
     return seq
 
 def dict_words(data_length):
-    """Find the maximum possible number of words in a dictionary built from a 
-    bitstring of the given length"""
+    """dict_words finds max possible number of words in a dictionary built from
+    bitstring of given length"""
     from numpy import ceil
     max_length = 1.
     sum_lengths = 2.
@@ -32,7 +32,7 @@ def dict_words(data_length):
     return int(final_words)
 
 def data_size(dict_size):
-    """Find the smallest bitstring length that could generate a dictionary of 
+    """data_size finds min bitstring length that could generate dictionary of
     given size"""
     dict_len = 1
     data_len = 1
@@ -42,19 +42,16 @@ def data_size(dict_size):
     return data_len
 
 def check_dict(data,dictionary,ii,build_dict = False):
-    """Input dataset building dictionary from/encoding by dictionary, current 
-    dictionary, current character in dataset, whether we want a longer word 
-    than is yet in the dictionary returned"""
-    """Start with a length of 1"""
+    """check_dict searches given data string, starting at position ii, for 
+    largest matching word in given dictionary; if build_dict, goes one char
+    farther"""
+    """km is string length compared to dictionary, length is string length 
+    checked for exceeding data length; only different if build_dict"""
     km = 1
     length = 1
     """Look for the longest word in the dictionary that matches the data"""
     while data[ii:ii+km] in dictionary:
         km += 1
-        """If we're adding new words to a dictionary, we want the returned word
-        to be longer than the last one that matched, but if we're looking up 
-        words in a static dictionary then we want to stick to things in the 
-        dictionary"""
         length = km - 1 + int(build_dict)
         """What to do for the string that reaches the end of the data"""
         if build_dict & (ii+length > len(data)):
@@ -64,14 +61,14 @@ def check_dict(data,dictionary,ii,build_dict = False):
     return data[ii:ii+length], length
 
 def bin_words(length, howMany):
-    "Creates a sequence of bit strings of a certain length"
+    "bin_words generates a sequence of bit strings of a certain length"
     length = int(length)
     howMany = int(howMany)
     for ii in range(howMany):
         yield bin(ii)[2:].zfill(length)
 
 class LZ78Dict(object):
-    """Initialize this object by breaking the given data into a dictionary"""
+    """Initialize LZ78Dict object by breaking the given data into a dictionary"""
     def __init__(self,data):
         data_length = len(data)
         dict_length_max = dict_words(data_length)
@@ -86,7 +83,7 @@ class LZ78Dict(object):
             bitno += bitstep
             self.encode_dict[next_w] = ward
         self.decode_dict = {v: k for k, v in self.encode_iter}
-    """Define length as number of entries in the dictionary"""
+    """Define __len__ as number of entries in the dictionary"""
     def __len__(self):
         return len(self.decode_dict)
     """Give length of the dictionaries as the __repr__ and __str__"""
@@ -94,11 +91,11 @@ class LZ78Dict(object):
         return "Encode & decode dictionaries of ",str(len(self))," entries"
     def __str__(self):
         return repr(self)
-    """Turn the encoding dictionary in to an iterable"""
+    """Turn the encoding dictionary into encode_iter iterable property"""
     @property
     def encode_iter(self):
         return self.encode_dict.iteritems()
-    """Run strings through the encoding dictionary"""
+    """encode method expresses given string in terms of encode dictionary"""
     def encode(self,message):
         ii = 0
         kryptos = ""
@@ -109,7 +106,7 @@ class LZ78Dict(object):
             kryptos += self.encode_dict[ward]
             ii += di
         return kryptos
-    """Run strings through the decoding dictionary"""
+    """decode method expresses given string in terms of decode dictionary"""
     def decode(self,coded):
         ii = 0
         original = ""
@@ -120,7 +117,7 @@ class LZ78Dict(object):
             ii += delta
         return original
 
-"""Test the dictWord, BernSeq, __init__, __len__, encode and decode functions & methods"""
+"""Run module as command to see demonstration of functionalities"""
 if __name__ == "__main__":
     max_key_len = 8
     print "Let's make a dictionary with ",max_key_len,"-bit keys out of a random bitstring"
